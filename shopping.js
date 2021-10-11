be5bb8aa5d8be8d6a54d9284962c75e329a77747
auntviv <aviva.mondesir@gmail.com>
2021-10-11 15:38:26 -0500

    Initial Commit

---
+++ shopping.js
@@ -0,0 +1,47 @@
+const food = [
+    {
+    id: 1,
+    name: "Milk",
+    price: 2.15
+    },
+
+    {
+    id: 2,
+    name: "Eggs",
+    price: 3.90
+    },
+
+    {
+    id: 3,
+    name: "Pizza Rolls",
+    price: 4.99
+    },
+
+    {
+    id: 4,
+    name: "Steak",
+    price: 14.87
+    },
+
+    {
+    id: 5,
+    name: "Watermelon",
+    price: 6.15
+    },
+
+    {
+    id: 6,
+    name: "Potato Bread",
+    price: 3.50
+    },
+
+    {
+    id: 7,
+    name: "Ketchup",
+    price: 5.00
+    }
+]
+
+
+
+console.log(food)
